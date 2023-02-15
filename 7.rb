cda8aed27a3158db424ebac21a45b18dd592e93f
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2023-02-15 13:39:14 +0000

    practice

---
+++ 7.rb
@@ -0,0 +1,12 @@
+def create_number(number)
+  number_list = []
+  for i in 1.upto(number) do
+    if (i % 7 == 0 )
+      number_list.append(i)
+    end
+  end
+  return number_list.sum{|i| i.to_s.count("7")}
+  # return number_list
+end
+
+puts "#{create_number(7777777)}"
