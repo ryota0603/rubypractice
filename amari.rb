92d5a641c9948af78a3dd4c11e4cd56c02f716f4
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2023-02-18 14:27:51 +0000

    rb

---
+++ amari.rb
@@ -0,0 +1,7 @@
+s = 1..2000
+t = 1..2000
+for i in 1..s do
+
+puts s % 
+i = i + 1
+end
---
+++ ifbun.rb
@@ -0,0 +1,6 @@
+# if文による条件分岐
+number = rand(1..3)
+puts "あなたの順位は#{number}位です"
+if number == 1
+  puts "おめでとう！"
+end
