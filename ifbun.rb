408ea6053dfea1e8bf93853e2dc3328f7ed72b99
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2023-02-18 14:28:28 +0000

    rb2

--- ifbun.rb
+++ ifbun.rb
@@ -1,6 +1,12 @@
 # if文による条件分岐
-number = rand(1..3)
+number = rand(1..5)
 puts "あなたの順位は#{number}位です"
 if number == 1
-  puts "おめでとう！"
+    puts "おめでとう！"
+    # １位だった時の処理
+elsif number == 2
+    puts "あと少し！"
+    # それ以外だったときの処理
+else
+    puts "よくがんばったね"
 end
