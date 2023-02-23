2a65027a8dcd5c0ebf8ee731276038fdd0620763
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2023-02-23 13:39:47 +0000

    paiza

---
+++ katu.rb
@@ -0,0 +1,12 @@
+# 自分の得意な言語で
+# Let's チャレンジ！！
+a,b = gets.split.map(&:to_i)
+if a == 0 && b == 0
+    puts 0
+elsif a == 0 && b == 1
+    puts 0
+elsif a == 1 && b == 0
+    puts 0
+elsif a == 1 && b == 1
+    puts 1
+end
