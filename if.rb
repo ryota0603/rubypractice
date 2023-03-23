595a5c38a7c628fd030fe434770179f5ab977591
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2023-03-23 16:10:36 +0000

    hikaku

--- if.rb
+++ if.rb
@@ -4,3 +4,14 @@ if s == "paiza"
 else
     puts "NO"
 end
+
+# 自分の得意な言語で
+# Let's チャレンジ！！
+a,b = gets.split.map(&:to_i)
+if a > b
+    puts a
+elsif b > a
+    puts b
+elsif b == a
+    puts "eq"
+end
